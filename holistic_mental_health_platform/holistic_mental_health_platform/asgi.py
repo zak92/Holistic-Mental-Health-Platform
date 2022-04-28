d97f75c553c19ea0e6bcee895a7c865a7009c2e0
Zakiyyah Ahmed <zaktech269@gmail.com>
2022-04-28 11:28:35 +0200

    Installed the necessary packages Initialized Django Project Created a new app - 'users' The 'users' app will enable user login and registration

---
+++ holistic_mental_health_platform/holistic_mental_health_platform/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for holistic_mental_health_platform project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'holistic_mental_health_platform.settings')
+
+application = get_asgi_application()
