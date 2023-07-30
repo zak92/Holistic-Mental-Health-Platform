038b0b3e00b57c07fdddc178de908cd8db4f03fa
Zakiyyah Ahmed <48877866+zak92@users.noreply.github.com>
2023-07-30 20:22:37 +0200

    Update settings.py

--- holistic_mental_health_platform/holistic_mental_health_platform/settings.py
+++ holistic_mental_health_platform/holistic_mental_health_platform/settings.py
@@ -181,8 +181,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # During developm
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'iotplantcare8266@gmail.com' 
-EMAIL_HOST_PASSWORD = 'bgaxwzzspsvmrnpk'  # APP PASSWORD - bgax wzzs psvm rnpk
+EMAIL_HOST_USER = '@gmail.com' 
+EMAIL_HOST_PASSWORD = ''  # APP PASSWORD 
 
 
 
